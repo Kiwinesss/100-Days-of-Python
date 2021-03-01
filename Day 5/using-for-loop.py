eb0b78a5203b4b3c07e59f0955f50826fdac2da0
Gazza <73530443+Kiwinesss@users.noreply.github.com>
2021-03-01 12:34:27 +0100

    Create using-for-loop.py

---
+++ Day 5/using-for-loop.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+  print(fruit)
+  print(fruit + " Pie") #indentation is important
+  print(fruits) #indented
+print(fruits) #not indented, see the difference in the result
