6b0db311e73683e91a145d7953e9ddce09da510b
Gazza <73530443+Kiwinesss@users.noreply.github.com>
2021-03-01 18:26:35 +0100

    Update adding_even_numbers.py

--- Day 5/adding_even_numbers.py
+++ Day 5/adding_even_numbers.py
@@ -1,7 +1,15 @@
 # adding the even numbers between 1 and 100 together
 
-even_score = 0
+even_num = 0
 for number in range(1,101):
   if number % 2 == 0:
-    even_score += number
-print(even_score)
+    even_num += number
+print(even_num)
+
+
+# second method
+
+even_num = 0
+for number in range(1,101,2):
+    even_num += number
+print(even_num)
