595fd789035bd4c822161389b22f411e3dea993e
Gazza <73530443+Kiwinesss@users.noreply.github.com>
2021-03-01 12:35:44 +0100

    Create average-height2.py

---
+++ Day 5/average-height2.py
@@ -0,0 +1,9 @@
+
+# this solution without using the sum and len functions
+import statistics
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+avg = statistics.mean(student_heights)
+print("The average student height is ", round(avg)) # print out the rounded result
