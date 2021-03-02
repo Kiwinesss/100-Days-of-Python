122577f015c83fec46fc2f8a011a2bfaccb22d4a
Gazza <73530443+Kiwinesss@users.noreply.github.com>
2021-03-02 11:08:20 +0100

    Update password_generator.py

--- Day 5/password_generator.py
+++ Day 5/password_generator.py
@@ -1,5 +1,7 @@
 #Password Generator Project
+
 import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -9,9 +11,7 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# create the password using a user-stipulated combination of random characters from the lists of letters, numbers and symbols
 
 characters = ""
 for letter in range(0,nr_letters):
@@ -23,7 +23,7 @@ for number in range(0,nr_numbers):
 for symbol in range(0,nr_symbols):
     characters += random.choice(symbols)
 
-print(characters)
+print(characters) # temporary command, will be removed when part two of the code is created
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+#randomise the order of the created password. WIP
+
