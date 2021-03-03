7a4cc7e74aebec1b2a610fd8c925b4b101d8f3e4
Gazza <73530443+Kiwinesss@users.noreply.github.com>
2021-03-03 10:36:51 +0100

    Create Reeborgs_world_hurdle_1.py

---
+++ Day 6/Reeborgs_world_hurdle_1.py
@@ -0,0 +1,22 @@
+#The first hurdle challenge at https://reeborg.ca/
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for step in range(6):
+    jump()
+
+
+ 
