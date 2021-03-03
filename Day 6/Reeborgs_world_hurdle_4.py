1223e2598641c246428b7d23185d66cf31855df6
Gazza <73530443+Kiwinesss@users.noreply.github.com>
2021-03-03 09:56:18 +0100

    Create Reeborgs_world_hurdle_4.py

---
+++ Day 6/Reeborgs_world_hurdle_4.py
@@ -0,0 +1,30 @@
+#The fourth hurdle challenge at https://reeborg.ca/
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    while not right_is_clear():
+        move()
+    else:
+        turn_right()
+    move()
+    turn_right()
+    move()
+    while not wall_in_front():
+        move()
+    else:
+        turn_left()
+
+while not at_goal():
+    if not front_is_clear():
+        jump()
+    elif not wall_in_front():
+        move()
+    else:
+        jump()
+
+ 
