515b2be8ddbe4b652e14f50aabeb48408817ad4c
Gazza McPherson <73530443+Kiwinesss@users.noreply.github.com>
2021-03-05 11:01:59 +0100

    Create hangman_part3.py

---
+++ Day 7/hangman_part3.py
@@ -0,0 +1,38 @@
+#Coding the third part of the hangman game
+
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+game_over = False #set a variable for the while loop
+
+while not game_over: #Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+  guess = input("Guess a letter: ").lower()
+
+  #Check guessed letter
+  for position in range(word_length):
+      letter = chosen_word[position]
+      
+      if letter == guess:
+        display[position] = letter
+
+  print(display)
+
+  if "_" not in display:
+    end_of_game = True
+    print("You win!")
+    
+    
+    
+    
