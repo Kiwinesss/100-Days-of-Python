99895db04f0a6d9b178acad16253b5b9daf90b98
Gazza McPherson <73530443+Kiwinesss@users.noreply.github.com>
2021-03-06 11:42:24 +0100

    Delete functions_positional.py

--- functions_positional.py
+++
@@ -1,7 +0,0 @@
-def greet_with(name, location):
-  print(f"Gidday {name}")
-  print(f"How is it in {location}?")
-
-greet_with("Gazza", "Heaven") #positional arguments - the order of the arguments cannot be changed
-
-
