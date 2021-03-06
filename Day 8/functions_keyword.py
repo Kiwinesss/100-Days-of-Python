ee9e5b3fc0f43d54175ddf496e10eec777a168b8
Gazza McPherson <73530443+Kiwinesss@users.noreply.github.com>
2021-03-06 11:43:49 +0100

    Create functions_keyword.py

---
+++ Day 8/functions_keyword.py
@@ -0,0 +1,12 @@
+def greet_with(name, location):
+  print(f"Gidday {name}")
+  print(f"How is it in {location}?")
+
+#greet_with("Gazza", "Heaven") #positional arguments - the order of the arguments cannot be changed
+
+greet_with(location="Heaven", name="Gazza") #keyword arguments, this way the order can be changed
+
+
+
+
+
