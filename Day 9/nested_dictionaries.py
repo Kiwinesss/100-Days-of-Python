65beab4d1263161b0fd64b5d8203d694b4856b74
Gazza McPherson <73530443+Kiwinesss@users.noreply.github.com>
2021-03-08 07:36:10 +0100

    Create nested_dictionaries.py

---
+++ Day 9/nested_dictionaries.py
@@ -0,0 +1,34 @@
+#nesting
+#a normal dictionary: 
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+#nesting a list in a dictionary
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+#nesting dictionary in a dictionary
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+#nesting dictionaries in lists
+travel_log = [
+{
+  "country": "France", 
+  "cities_visited": ["Paris", "Lille", "Dijon"], 
+  "total_visits": 12,
+},
+{
+  "country": "Germany",
+  "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+  "total_visits": 5,
+},
+]
+
+
